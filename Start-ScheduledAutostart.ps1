8b2ee0e650c4a3758d917109b2feb8da9caa082a
Matthias Albrecht <matthias.albrecht@uni-konstanz.de>
2022-08-26 18:45:04 +0200

    Implement scheduled stop and restart

--- Start-ScheduledAutostart.ps1
+++ Start-ScheduledAutostart.ps1
@@ -1,20 +1,83 @@
 param (
-    $StartTime = '09:00',
-    $EndTime = '18:00',
-    $DaysOff = @('Saturday', 'Sunday')
+    $StartTime = '06:00',
+    $EndTime = '17:00',
+    $DaysOff = @('Saturday', 'Sunday'),
+    $Programs = @('C:\Users\biosmanager\AppData\Local\slack\slack.exe', 'C:\Users\biosmanager\AppData\Local\Programs\mattermost-desktop\Mattermost.exe'),
+    [switch]$ScheduleStop = $True,
+    [switch]$ScheduleRestart = $False
 )
 
-$StartTimeDate = Get-Date $StartTime
-$EndTimeDate = Get-Date $EndTime
-$Date = Get-Date
+Import-Module PSScheduledJob
 
-if ($Date.DayOfWeek -notin $DaysOff -and (($Date.TimeOfDay -ge $StartTimeDate.TimeOfDay) -and ($Date.TimeOfDay -le $EndTimeDate.TimeOfDay))) {
-    # Autostart
-    Write-Output 'Another day, another dollar!'
+function Start-AutostartApplications {
+    foreach ($Program in $Programs) {
+        Start-Process $Program -RedirectStandardOutput nul
+    }
+}
 
-    Start-Process '<program1>' -RedirectStandardOutput nul
-    Start-Process '<program2>' -RedirectStandardOutput nul
+function Stop-AutostartApplications {
+    foreach ($Program in %Programs) {
+        Get-Process | Where-Object { $_.Path -like $Program } | Stop-Process
+    }
+}
+
+function Start-ScheduledAutostartApplications {
+    $StartTimeDate = Get-Date $StartTime
+    $EndTimeDate = Get-Date $EndTime
+    $Date = Get-Date
+    
+    if ($Date.DayOfWeek -notin $DaysOff -and (($Date.TimeOfDay -ge $StartTimeDate.TimeOfDay) -and ($Date.TimeOfDay -le $EndTimeDate.TimeOfDay))) {
+        # Autostart
+        Write-Output 'Another day, another dollar!'
+    
+        Start-AutostartApplications
+    }
+    else {
+        Write-Output 'Enjoy your freetime :)'
+    }
+}
+
+Start-ScheduledAutostartApplications
+
+if (($null -eq $IsWindows) -or $IsWindows) {
+    if ($PSVersionTable.PSVersion.Major -le 5) {
+        # Schedule job to reduce work distractions if the computer is still on
+        if ($ScheduleStop) {
+            $StateChangeTrigger = Get-CimClass `
+                -Namespace ROOT\Microsoft\Windows\TaskScheduler `
+                -ClassName MSFT_TaskSessionStateChangeTrigger
+            $OnUnlockTrigger = New-CimInstance `
+                -CimClass $StateChangeTrigger `
+                -Property @{
+                StateChange = 8  # TASK_SESSION_STATE_CHANGE_TYPE.TASK_SESSION_UNLOCK (taskschd.h)
+            } ` -ClientOnly
+        
+            $StopJobName = 'ScheduledAutostart-StopAutostartApplicationsAfterEndOfWorkingDay'
+            Register-ScheduledJob -Name $StopJobName -ScriptBlock {
+                $Date = Get-Date
+                if ($Date.DayOfWeek -notin $DaysOff -and $Date.TimeOfDay -ge $EndTimeDate.TimeOfDay) {
+                    Stop-AutostartApplications
+                }
+            }
+
+            $Task = Get-ScheduledTask -TaskPath "\Microsoft\Windows\PowerShell\ScheduledJobs\" -TaskName $StopJobName
+            $Task | Set-ScheduledTask -Trigger $OnUnlockTrigger
+        }
+       
+        if ($ScheduleRestart) {
+            # Restart autostart applications on the next working day if the computer is still on
+            $RestartJobName = 'ScheduledAutostart-RestartAutostartApplicationsOnNewWorkingDay'
+            $RestartTimeDate = Get-Date $StartTime
+            $RestartJobTrigger = New-JobTrigger -Daily -At $RestartTimeDate
+            Register-ScheduledJob -Name $RestartJobName -Trigger $RestartJobTrigger -ScriptBlock {
+                Start-ScheduledAutostartApplications
+            }
+        }
+    }
+    else {
+        Write-Warning 'Job scheduler only works in Windows PowerShell 5 and below!'
+    }
 }
 else {
-    Write-Output 'Enjoy your freetime :)'
+    Write-Warning 'Job scheduler only works on Windows!'
 }
